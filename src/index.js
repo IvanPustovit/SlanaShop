8dac14df31d2f3ffe3de0cb428aa318d1286fead
IvanPustovit <pustovit.for@gmail.com>
2020-05-21 14:00:04 +0300

    SlanaShop_update_router

--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ import App from "./App";
 import { Provider } from "react-redux";
 import store from "./redux/store/store";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
+        {/* <BrowserRouter> */}
         <App />
-      </BrowserRouter>
+        {/* </BrowserRouter> */}
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root"),
